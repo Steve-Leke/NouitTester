e7b7eca7054cea7c3c8c843dde4a466dbc1bc28c
Steve <stevel@csedge.org>
2020-07-21 17:49:32 -0700

    Initial commit

---
+++ app/src/main/java/com/example/nouittester/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nouittester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
